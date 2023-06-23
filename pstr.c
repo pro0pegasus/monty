9216593bca54ac8ea82e2650b85b81e443c84cfe
BORAK <yassineborak@gmail.com>
2023-06-22 17:36:39 -0700

    commit

---
+++ pstr.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+*f_pstr - It prints the str starting from the top of the stack,
+*followed by new
+*@head: stack head
+*@counter: line_number
+*Return: no return
+*/
+void f_pstr(stack_t **head, unsigned int counter)
+
+{
+
+		stack_t *h;
+
+		(void)counter;
+
+
+
+		h = *head;
+
+		while (h)
+
+		{
+
+			if (h->n > 127 || h->n <= 0)
+
+			{
+
+				break;
+
+			}
+
+			printf("%c", h->n);
+
+			h = h->next;
+
+		}
+
+		printf("\n");
+
+}
